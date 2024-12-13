b5610c8e40910e373969c507bfdc994fba89b19e
Halatnikov <halatnikovpav@yandex.ru>
2024-12-14 04:53:14 +0700

    0.3.1-22 more refactoring

---
+++ source/srbftp.capx/Files/main.js
@@ -0,0 +1,30 @@
+var srbftp = {};
+
+///////////////////////////////////////////////////////////////
+
+String.format = function () {
+	let string = arguments[0];
+
+	for (let i = 1; i < arguments.length; i++) {
+		string = string.replace(/{[A-Za-z0-9]+}/, arguments[i]);
+	};
+
+	return string;
+};
+
+///////////////////////////////////////////////////////////////
+
+var proxies = {};
+
+// workarounds so that js.storage returns construct-readable jsons
+proxies.localStorage_get = Storages.localStorage.get;
+Storages.localStorage.get = function() {
+	let proxied = proxies.localStorage_get.apply(this, arguments)
+	return JSON.stringify(proxied);
+};
+
+proxies.localStorage_keys = Storages.localStorage.keys;
+Storages.localStorage.keys = function() {
+	let proxied = proxies.localStorage_keys.apply(this, arguments)
+	return JSON.stringify(proxied);
+};
