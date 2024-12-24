06b53f8717a8d759ecb6278d0a06fbb5a99ea470
Halatnikov <halatnikovpav@yandex.ru>
2024-12-24 09:14:07 +0700

    0.3.1-23 sonic is hacking your copmuter

--- source/srbftp.capx/Files/main.js
+++ source/srbftp.capx/Files/main.js
@@ -19,12 +19,12 @@ var proxies = {};
 // workarounds so that js.storage returns construct-readable jsons
 proxies.localStorage_get = Storages.localStorage.get;
 Storages.localStorage.get = function() {
-	let proxied = proxies.localStorage_get.apply(this, arguments)
+	let proxied = proxies.localStorage_get.apply(this, arguments);
 	return JSON.stringify(proxied);
 };
 
 proxies.localStorage_keys = Storages.localStorage.keys;
 Storages.localStorage.keys = function() {
-	let proxied = proxies.localStorage_keys.apply(this, arguments)
+	let proxied = proxies.localStorage_keys.apply(this, arguments);
 	return JSON.stringify(proxied);
 };
